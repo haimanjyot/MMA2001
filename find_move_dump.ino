9992917beb450d39c9abdb7cd16b3bd85dc8ca5f
Haiman <haimanjyot.m@gmail.com>
2026-05-06 01:45:19 +1000

    dsvgbvf

--- find_move_dump.ino
+++ find_move_dump.ino
@@ -117,7 +117,7 @@ void loop() {
 
 }
 
-void doServoStuff(int servoNo, float degree, int delayTime = 0) { // Equating it to 0 means the deafult delay is 0, remove if it stops working
+void doServoStuff(int servoNo, float degree, int delayTime) {
     if (servoNo == 1) {
       armServo1.write(degree);
     } 
